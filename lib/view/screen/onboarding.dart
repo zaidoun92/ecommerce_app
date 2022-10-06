08e9b271bdd29ae16f391eb3de19050d7dbf0ca1
zaidoun92 <27ew75ma@gmail.com>
2022-10-06 08:22:53 +0300

    Start On Barding Page

---
+++ lib/view/screen/onboarding.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class OnBoarding extends StatelessWidget {
+  const OnBoarding({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(10),
+      ),
+    );
+  }
+}
