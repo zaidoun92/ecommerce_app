cb256eceddad29098af99e5121ff897fdd758f24
zaidoun92 <27ew75ma@gmail.com>
2022-10-06 08:42:25 +0300

    add onboardingModel , imageasset and static

---
+++ lib/core/constant/imageasset.dart
@@ -0,0 +1,9 @@
+class ImageAsset {
+  //
+  static const String rootImages = "assets/images";
+
+  static const String onBoardingImageOne = "$rootImages/onboardingone.PNG";
+  static const String onBoardingImageTwo = "$rootImages/onboardingtwo.PNG";
+  static const String onBoardingImageThree = "$rootImages/onboardingthree.PNG";
+  static const String onBoardingImageFour = "$rootImages/onboardingfour.PNG";
+}
---
+++ lib/data/datasource/static/static.dart
@@ -0,0 +1,29 @@
+import 'package:ecommerce_app/core/constant/imageasset.dart';
+import 'package:ecommerce_app/data/model/onboardingmodel.dart';
+
+List<OnBoardingModel> onBoardingList = [
+  OnBoardingModel(
+    title: "Choose Product",
+    body:
+        "We Have a 100k Product , Choose \n Your product from our E-Commerce Shop",
+    image: ImageAsset.onBoardingImageOne,
+  ),
+  OnBoardingModel(
+    title: "Easy And Safe Payment",
+    body:
+        "We Have a 100k Product , Choose \n Your product from our E-Commerce Shop",
+    image: ImageAsset.onBoardingImageTwo,
+  ),
+  OnBoardingModel(
+    title: "Track Your Order",
+    body:
+        "We Have a 100k Product , Choose \n Your product from our E-Commerce Shop",
+    image: ImageAsset.onBoardingImageThree,
+  ),
+  OnBoardingModel(
+    title: "Fast Delivery",
+    body:
+        "We Have a 100k Product , Choose \n Your product from our E-Commerce Shop",
+    image: ImageAsset.onBoardingImageFour,
+  ),
+];
